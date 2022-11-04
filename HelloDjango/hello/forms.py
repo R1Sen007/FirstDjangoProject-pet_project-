4ca184536e8877380589b8c6a98abc849d254ee1
R1Sen <ptyrtin@mail.ru>
2022-11-04 12:49:54 +0300

    make fist model and migration then apply it to db

--- HelloDjango/hello/forms.py
+++ HelloDjango/hello/forms.py
@@ -3,3 +3,5 @@ from django import forms
 class UserForm(forms.Form):
     name = forms.CharField()
     age = forms.IntegerField()
+    languages = forms.ChoiceField(choices=((1, "English"), (2, "German"), (3, "French")))
+    password = forms.CharField(widget= forms.PasswordInput)

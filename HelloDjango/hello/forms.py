60521262917c72dc52f602a8ea17e698ab82d175
R1Sen <ptyrtin@mail.ru>
2022-11-08 13:29:03 +0300

    Change form to ModelForm

--- HelloDjango/hello/forms.py
+++ HelloDjango/hello/forms.py
@@ -1,7 +1,16 @@
 from django import forms
+from .models import Person
 
-class UserForm(forms.Form):
-    name = forms.CharField()
-    age = forms.IntegerField()
-    # languages = forms.ChoiceField(choices=((1, "English"), (2, "German"), (3, "French")))
-    # password = forms.CharField(widget= forms.PasswordInput)
+# class UserForm(forms.Form):
+#     name = forms.CharField()
+#     age = forms.IntegerField()
+#     languages = forms.ChoiceField(choices=((1, "English"), (2, "German"), (3, "French")))
+#     password = forms.CharField(widget= forms.PasswordInput)
+
+class UserForm(forms.ModelForm):
+    class Meta:
+        model = Person
+        fields = [
+            'name',
+            'age'
+        ]

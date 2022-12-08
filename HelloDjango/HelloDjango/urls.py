18bc3c13a6c39c270222fcfb25930567c8181970
R1Sen <ptyrtin@mail.ru>
2022-12-08 10:56:06 +0300

    add profile picture

--- HelloDjango/HelloDjango/urls.py
+++ HelloDjango/HelloDjango/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from hello import views as hello_views
 from users import views as user_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 # products_patterns = [
 #     path("", views.products),
@@ -49,3 +51,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('signin/', user_views.register, name='signin')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- HelloDjango/users/models.py
+++ HelloDjango/users/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # img = ???
-
+    image = models.ImageField(default='default.jpg', upload_to='profile_pic')
+    
+    
     def __str__(self):
         return " ".join([self.user.first_name, self.user.last_name])
 
+    def get_mini_profile_pic(self):
+        img = Image.open(self.image.path)
+        img.thumbnail((300,300))
+        
     
 
 # Create your models here.
